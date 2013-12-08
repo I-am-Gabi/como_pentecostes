4a48bd88a325cf372d51696acad6a1741f00c0bf
Gabriela C. da Silva <gabicavalcantesilva@gmail.com>
2013-12-08 17:00:14 -0300

    Atualizado

---
+++ 2013 - 2/js/secope.main.js
@@ -0,0 +1,7 @@
+/*jQuery(document).ready(function(){
+	jQuery(window).scroll(function(){
+		var scrollSpeed = 0.5;
+		var yPos = -(parseInt($(window).scrollTop()*scrollSpeed));
+        jQuery(".jumbotron").css("background-position", "left " + yPos + "px");
+	});
+});*/
